944bc62420797ecaacc6f7a58085fb3bc645cc40
zeusjr237 <djipetakam@gmail.com>
2023-09-16 18:17:39 +0200

    Deployed website

--- src/components/LogoPage.jsx
+++ src/components/LogoPage.jsx
@@ -7,12 +7,12 @@ const LogoPage = () => {
   return (
     <div className='bg-primary min-h-screen overflow-hidden'>
       <div className='container mx-auto'>
-        <div className='flex lg:mx-[96rem] mx-[16rem] py-6'>
+        <div className='flex lg:mx-[72rem] mx-[16rem] py-6'>
            <Link to='/signin'
              spy={'false'}
             smooth={"true"}
             className=''>
-              <BsArrowRightShort className='text-xl transition-all ease-out duration-700'/>
+              <BsArrowRightShort className='text-4xl transition-all ease-out duration-700 animate-bounce'/>
             </Link>
           </div>
         <div className='text-3xl flex items-center justify-center my-[10rem]'>
