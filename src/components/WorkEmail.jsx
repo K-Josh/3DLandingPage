5a7ddfbafe551ada4df90b7b789af31468aef3d5
zeusjr237 <djipetakam@gmail.com>
2023-09-16 17:27:16 +0200

    Deployed website

--- src/components/WorkEmail.jsx
+++ src/components/WorkEmail.jsx
@@ -7,17 +7,17 @@ const WorkEmail = () => {
     <div className='banner bg-cover bg-no-repeat h-screen bg-tertiary mx-auto lg:h-[42rem] py-[20rem] overflow-x-hidden overflow-y-hidden' id='work_mail'>
         <div className='container -mt-[8rem] lg:-mt-[14rem]' >
              {/* work-email and button and shdow*/}
-             <div className='lg:mt-6 mt-2 flex flex-col lg:mb-20 lg:space-y-8 space-y-3 items-center border lg:mx-[22rem] mx-8 p-12 w-[20rem] lg:w-[38rem]'>
-                <h1 className='text-2xl font-secondary text-secondary/100 mb-6'>Your work email</h1>
+             <div className='lg:mt-6 mt-2 flex flex-col lg:mb-20 lg:space-y-8 space-y-3 items-center border lg:mx-[28rem] -mx-10 p-12 w-[18rem] lg:w-[28rem]'>
+                <h1 className='text-2xl font-secondary text-secondary/100 mb-6 whitespace-nowrap'>Your work email</h1>
               <input
                 type='email' 
-                className='px-6 rounded-2xl p-3 border-0 -mx-10 font-bold shadow-md outline-1 outline-secondary  text-black placeholder:text-gray-500/75 font-primary hover:shadow-lg w-[16rem] lg:w-[24rem]'
+                className='px-6 rounded-2xl p-3 border-0 -mx-10 font-bold shadow-md outline-1 outline-secondary  text-black placeholder:text-gray-500/75 font-primary hover:shadow-lg w-[14rem] lg:w-[24rem]'
                 placeholder='work email address'
               />
               <div>
               <button 
                 type='submit' 
-                className='btn lg:btn_2 btn_3_5 mt-8 bg-secondary border-0 -mx-10 font-bold   text-white font-primary hover:text-black '>
+                className='lg:btn lg:btn_2 px-8 py-3 rounded-2xl mt-8 bg-secondary border-0 -mx-10 font-bold   text-white font-primary hover:text-black '>
                Email me a sign up link
               </button>
               </div>
